f2685231343d78791c9e668b0218c36485425fd3
willherzog <willherzog@gmail.com>
2023-05-15 15:14:45 -0600

    Add GenericBag interface

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
  Some general PHP utilities:
  * `StringUtil`, `ArrayUtil` and `IntlDateUtil` with various static utility methods
  * `InvalidArgumentTypeException`, which generates an automatic, type-based error message
- * `LabelAwareEnum` interface, requiring an enum to have a `getLabel()` method
+ * `LabelAwareEnum` interface, requiring an enumerator to have a `getLabel()` method
+ * `GenericBag` interface
---
+++ src/Bag/GenericBag.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace WHPHP\Bag;
+
+/**
+ * Generic interface for "bag" classes.
+ * Inspired by PropertyBag from symfony/http-foundation.
+ *
+ * @author Will Herzog <willherzog@gmail.com>
+ */
+interface GenericBag extends \Iterator
+{
+	/**
+	 * Adds an item to the bag.
+	 *
+	 * @param $name String key to use for the item
+	 * @param $item The item itself
+	 *
+	 * @return Whether the item could be added (e.g. whether $name is already in use)
+	 */
+	public function add(string $name, mixed $item): bool;
+
+	/**
+	 * Removes an item from the bag.
+	 *
+	 * @param $name The item to remove
+	 *
+	 * @return Whether the item could be removed (e.g. whether the item exists)
+	 */
+	public function remove(string $name): bool;
+
+	/**
+	 * Determines whether an item is in the bag.
+	 *
+	 * @param $name The item to check
+	 *
+	 * @return Whether the item exists
+	 */
+	public function has(string $name): bool;
+
+	/**
+	 * Fetches an item from the bag.
+	 *
+	 * @param $name The item to fetch
+	 *
+	 * @return mixed The requested item or NULL if not present
+	 */
+	public function get(string $name);
+
+	/**
+	 * Returns all items in the bag as a plain array.
+	 */
+	public function all(): array;
+
+	/**
+	 * Determines whether or not this bag is empty.
+	 *
+	 * @return Whether this bag contains any items
+	 */
+	public function isEmpty(): bool;
+}
