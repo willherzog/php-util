e8b52d010c7d7f6546d4c2855370e1a620d72e82
Will Herzog <willherzog@gmail.com>
2025-06-16 13:50:17 -0600

    Add method descriptions

--- src/Util/DateTimeUtil.php
+++ src/Util/DateTimeUtil.php
@@ -16,11 +16,19 @@ final class DateTimeUtil
 	private function __construct()
 	{}
 
+	/**
+	 * Get the current time for the given timezone (UTC by default).
+	 */
 	static public function getCurrentTime(string $timezone = 'UTC'): DateTimeInterface
 	{
 		return new DateTimeImmutable('now', new DateTimeZone($timezone));
 	}
 
+	/**
+	 * Get the current UTC time formatted as a string (W3C format by default).
+	 *
+	 * @uses `self::getCurrentTime()`
+	 */
 	static public function getCurrentTimeString(string $format = DateTimeInterface::W3C): string
 	{
 		return self::getCurrentTime()->format($format);
