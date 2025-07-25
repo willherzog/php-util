5786a8fa42f7368e0a1ff034665ca277012dfec4
Will Herzog <willherzog@gmail.com>
2025-07-25 16:46:59 -0600

    Allow specifying multiple expected types
    and add $previous argument (matching that of InvalidArgumentException)

--- src/Exception/InvalidArgumentTypeException.php
+++ src/Exception/InvalidArgumentTypeException.php
@@ -9,10 +9,36 @@ namespace WHPHP\Exception;
  */
 class InvalidArgumentTypeException extends \InvalidArgumentException
 {
-	public function __construct(mixed $value, string $requiredType, int $code = 0)
+	/**
+	 * @param mixed $value The value with an invalid type
+	 * @param string|string[] $requiredType The expected type or types
+	 */
+	public function __construct(mixed $value, string|array $requiredType, int $code = 0, \Throwable|null $previous = null)
 	{
-		$valueType = \get_debug_type($value);
+		if( $requiredType === '' || $requiredType === [] ) {
+			throw new \LogicException('$requiredType must not be empty.');
+		}
 
-		parent::__construct(sprintf('Expected an argument of type "%s", got a(n) "%s" instead.', $requiredType, $valueType), $code);
+		$valueType = get_debug_type($value);
+		$quotify = fn($type) => '"'.$type.'"';
+
+		if( is_array($requiredType) ) {
+			$requiredTypes = array_values($requiredType); // ensure the array is zero-indexed
+
+			if( count($requiredTypes) > 1 ) {
+				array_walk($requiredTypes, $quotify);
+
+				$requiredType = implode(', ', array_slice($requiredTypes, 0, -1));
+				$requiredType .= ' or ' . $requiredTypes[array_key_last($requiredTypes)];
+			} else {
+				$requiredType = $quotify($requiredTypes[0]); // don't use multi-type message if there's actually only one type
+			}
+		} else {
+			$requiredType = $quotify($requiredType);
+		}
+
+		$message = sprintf('Expected an argument of type %s but got a(n) %s instead.', $requiredType, $quotify($valueType));
+
+		parent::__construct($message, $code, $previous);
 	}
 }
